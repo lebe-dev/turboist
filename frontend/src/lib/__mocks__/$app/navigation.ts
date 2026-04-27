0ee7bd25880ddefa75227f7d69c97dd75aca04df
Eugene Lebedev <duke.tougu@gmail.com>
2026-04-27 19:34:45 +0300

    Remove old implementation

--- frontend/src/lib/__mocks__/$app/navigation.ts
+++
@@ -1,8 +0,0 @@
-export const goto = () => Promise.resolve();
-export const invalidate = () => Promise.resolve();
-export const invalidateAll = () => Promise.resolve();
-export const preloadData = () => Promise.resolve();
-export const preloadCode = () => Promise.resolve();
-export const beforeNavigate = () => {};
-export const afterNavigate = () => {};
-export const onNavigate = () => {};
--- internal/storage/migrations/005_create_constraints.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS constraints_label_blocks (
-    label      TEXT NOT NULL PRIMARY KEY,
-    started_at TEXT NOT NULL
-);
